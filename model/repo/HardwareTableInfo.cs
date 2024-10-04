168f801284aa91ef6281bc21a0cfe2e568e64635
Conor-Harney <cw208kccgdbc00155687@gmail.com>
2024-10-04 18:21:18 +0100

    Base structure (#1)
    Base structure without testing and very basic UI

---
+++ model/repo/HardwareTableInfo.cs
@@ -0,0 +1,43 @@
+﻿namespace WindowsHardwareFinder.model.repo
+{
+    //ref https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/computer-system-hardware-classes
+    public enum ComputerSystemHardwareClass
+    {
+        Win32_Fan,
+        Win32_HeatPipe,
+        Win32_Refrigeration,
+        Win32_TemperatureProbe,
+
+        Win32_DesktopMonitor,
+        Win32_DisplayControllerConfiguration,
+        Win32_VideoController,
+        Win32_VideoSettings
+
+    }
+
+    public enum HardwareObject
+    {
+        CoolingDevice,
+        InputDevice,
+        MassStorage,
+        MotherboardControllerAndPort,
+        NetworkingDevice,
+        Power,
+        Printing,
+        Telephony,
+        VideoAndMonitor
+    }
+
+    internal class HardwareTableInfo
+    {
+        //Add entries for new Hardware Objects
+        protected static Dictionary<HardwareObject, List<ComputerSystemHardwareClass>> HardwareObjectRelatedTables = new Dictionary<HardwareObject, List<ComputerSystemHardwareClass>>()
+        {
+            { HardwareObject.CoolingDevice, new List<ComputerSystemHardwareClass> { ComputerSystemHardwareClass.Win32_Fan, ComputerSystemHardwareClass.Win32_HeatPipe, ComputerSystemHardwareClass.Win32_Refrigeration, ComputerSystemHardwareClass.Win32_TemperatureProbe } },
+            { HardwareObject.InputDevice, new List<ComputerSystemHardwareClass> { ComputerSystemHardwareClass.Win32_DesktopMonitor, ComputerSystemHardwareClass.Win32_DisplayControllerConfiguration, ComputerSystemHardwareClass.Win32_VideoController, ComputerSystemHardwareClass.Win32_VideoSettings } }
+        };
+
+        // Populated from repo calls as and when needed 
+        protected static Dictionary<ComputerSystemHardwareClass, List<string>> TableFields { get; } = new Dictionary<ComputerSystemHardwareClass, List<string>>();
+    }
+}
---
+++ view/parser/HardwareTableParser.cs
@@ -0,0 +1,61 @@
+﻿using System.Management;
+using WindowsHardwareFinder.model.repo;
+using WindowsHardwareFinder.model.user;
+using WindowsHardwareFinder.modelView.repo;
+using WindowsHardwareFinder.viewmodel.util;
+
+namespace WindowsHardwareFinder.view.parser
+{
+    internal static class HardwareTableParser
+    {
+        public static string UserFriendlyTable(HardwareObject hardwareObject)
+        {
+            HardwareObjectData hardwareObjectData = ParseHardwareObject(hardwareObject);
+            String data = hardwareObjectData.Name + ":\r\n";
+            foreach (ComputerSystemHardwareClassData classData in hardwareObjectData.ComputerSystemHardwareClasses)
+            {
+                data += "\t" + classData.Name + ":\r\n";
+                foreach (string description in classData.Descriptions)
+                {
+                    data += "\t\t" + description + "\r\n";
+                }
+            }
+            return data;
+        }
+        
+        public static HardwareObjectData ParseHardwareObject(HardwareObject hardwareObject) 
+        {
+            HardwareObjectData hardwareObjectData = new HardwareObjectData(hardwareObject);
+            foreach (ComputerSystemHardwareClass table in HardwareTableInfoUtil.GetTablesForObject(hardwareObject))
+            {
+                ManagementObjectCollection rows = HardwareObjectRepo.queryAll(table);
+                ComputerSystemHardwareClassData computerSystemHardwareClassData = ParseRows(rows, table);
+                hardwareObjectData.ComputerSystemHardwareClasses.Add(computerSystemHardwareClassData);
+            }
+            return hardwareObjectData;
+        }
+
+        private static ComputerSystemHardwareClassData ParseRows(ManagementObjectCollection rows, ComputerSystemHardwareClass computerSystemHardwareClass)
+        {
+            string fieldToFind = "Description";
+            ComputerSystemHardwareClassData computerSystemHardwareClassData = new ComputerSystemHardwareClassData(computerSystemHardwareClass);
+            if (HardwareTableInfoUtil.TableHasProperty( computerSystemHardwareClass, fieldToFind))
+            {
+                foreach (ManagementObject row in rows)
+                {
+
+                    try
+                    {
+                        var description = row[fieldToFind];
+                        computerSystemHardwareClassData.Descriptions.Add((string)description);
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+                }
+            }
+            return computerSystemHardwareClassData;
+        }
+    }
+}
